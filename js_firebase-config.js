dc73626137b510a18fb5f25e84654fd31933a9c6
trishanth <trishanthaditya@gmail.com>
2026-08-23 17:44:20 +0000

    init commit

---
+++ js_firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your exact config block from your Firebase Console here
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY_HERE",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+console.log("Firebase context framework successfully mounted locally!");
